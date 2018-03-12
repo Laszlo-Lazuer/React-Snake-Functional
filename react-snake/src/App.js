62227567cf0516389b01b7961e0668647b865ae9
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-12 02:27:19 -0500

    Adds event listener to move snake.

--- react-snake/src/App.js
+++ react-snake/src/App.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import cs from 'classnames';
 
+const TICK_RATE = 100;
 const GRID_SIZE = 35;
 const GRID = [];
+const KEY_CODES_MAPPER = {
+  38: 'UP',
+  39: 'RIGHT',
+  37: 'LEFT',
+  40: 'BOTTOM',
+};
 
 for (let i=0; i <= GRID_SIZE; i++) {
   GRID.push(i);
 }
+
+const DIRECTIONS = {
+  RIGHT: 'RIGHT',
+  LEFT: 'LEFT',
+  UP: 'UP',
+  BOTTOM: 'BOTTOM',
+};
+
+const DIRECTION_TICKS = {
+  UP: (x,y) => ({ x, y: y - 1 }),
+  BOTTOM: (x,y) => ({ x, y: y + 1 }),
+  RIGHT: (x,y) => ({ x: x + 1, y: y}),
+  LEFT: (x,y) => ({ x: x -1, y: y}),
+};
 
 const isBorder = (x,y) =>
   x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE;
@@ -22,11 +43,34 @@ const getCellCS = (snake, snack, x,y) =>
     'grid-cell-snack': isPosition(x, y, snack.coordinate.x, snack.coordinate.y),
   });
 
+  const doChangeDIrection = (direction) => () => ({
+    playground: {
+      direction
+    }
+  });
+
+  const applySnakePosition = prevState => {
+    let x;
+    let y;
+
+    const directionFn = DIRECTION_TICKS[prevState.playground.direction];
+    const coordinate = directionFn(prevState.snake.coordinate.x, prevState.snake.coordinate.y);
+
+    return {
+      snake: {
+        coordinate,
+        },
+    };
+  };
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      playground: {
+        direction: DIRECTIONS.RIGHT,
+      },
       snake: {
         coordinate: {
           x: 20,
@@ -40,6 +84,30 @@ class App extends Component {
         }
       }
     };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.onTick, TICK_RATE);
+
+    window.addEventListener('keyup', this.onChangeDirection, true);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+
+    window.removeEventListener('keyup', this.onChangeDirection, false);
+  }
+
+  onTick = () => {
+    //Move snake
+    this.setState(applySnakePosition)
+  }
+
+  onChangeDirection = (event) => {
+    const direction = KEY_CODES_MAPPER[event.keyCode];
+    if (direction) {
+        this.setState(doChangeDIrection(direction));
+    }
   }
 
   render() {
