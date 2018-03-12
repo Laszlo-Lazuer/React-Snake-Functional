1c6ba80d47d7e6c5613f4474361e1363f0bc4eb2
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-12 02:04:35 -0500

    Adds snake and snack to canvas.

--- react-snake/src/App.js
+++ react-snake/src/App.js
@@ -12,51 +12,80 @@ for (let i=0; i <= GRID_SIZE; i++) {
 const isBorder = (x,y) =>
   x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE;
 
-const getCellCS = (x,y) =>
+const isPosition = (x, y, diffX, diffY) =>
+  x === diffX && y === diffY;
+
+const getCellCS = (snake, snack, x,y) =>
   cs('grid-cell', {
     'grid-cell-border': isBorder(x,y),
+    'grid-cell-snake': isPosition(x, y, snake.coordinate.x, snake.coordinate.y),
+    'grid-cell-snack': isPosition(x, y, snack.coordinate.x, snack.coordinate.y),
   });
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      snake: {
+        coordinate: {
+          x: 20,
+          y: 10,
+        }
+      },
+      snack: {
+        coordinate: {
+          x: 25,
+          y: 10,
+        }
+      }
+    };
   }
 
   render() {
+    const {
+      snake,
+      snack,
+    } = this.state;
     return (
       <div className="app">
       <h1>Snake!</h1>
-      <Grid />
+      <Grid
+        snake={snake}
+        snack={snack}
+        />
       </div>
     );
   }
 }
 
-const Grid = () =>
+const Grid = ({snake, snack}) =>
 <div>
   {GRID.map(y =>
     <Row
       y={y}
       key={y}
+      snake={snake}
+      snack={snack}
       />
   )}
 </div>
 
-const Row = ({ y }) =>
+const Row = ({ snake, snack, y }) =>
   <div className="grid-row">
     {GRID.map(x =>
       <Cell
         x={x}
         y={y}
         key={x}
+        snake={snake}
+        snack={snack}
       />
     )}
   </div>
 
-  const Cell = ({ x,y }) =>
-    <div className={getCellCS(x,y)} />
+  const Cell = ({ snake, snack, x,y }) =>
+    <div className={getCellCS(snake, snack, x,y)} />
 
 
 export default App;
